b42305dbb04d182bb4f52a3bd311642142ae0b51
afterwise <afterwise@gmail.com>
2014-07-30 15:14:11 +0200

    No longer just a header file, to avoid unused static function warnings

--- aw-wav.h
+++ aw-wav.h
@@ -27,10 +27,8 @@
 #include "aw-endian.h"
 
 #if __GNUC__
-# define _wav_alwaysinline inline __attribute__((always_inline))
 # define _wav_packed __attribute__((packed))
 #elif _MSC_VER
-# define _wav_alwaysinline __forceinline
 # define _wav_packed
 #endif
 
@@ -82,86 +80,8 @@ struct _wav_packed wav_format {
 # pragma pack(pop)
 #endif
 
-static int wav_parse(struct wav_info *info, const void *data) {
-	const struct wav_chunk *chunk = data;
-	const struct wav_riff *riff = (const void *) &chunk[1];
-	const struct wav_format *format;
-	u32 chunk_id;
-	u32 chunk_size;
-
-	if (btoh32(chunk->id) != wav_fourcc('R', 'I', 'F', 'F'))
-		return -1;
-
-	if (btoh32(riff->format) != wav_fourcc('W', 'A', 'V', 'E'))
-		return -2;
-
-	chunk = (const void *) &riff[1];
-
-	for (;;) {
-		chunk_id = btoh32(chunk->id);
-		chunk_size = ltoh32(chunk->size);
-
-		if (chunk_id == wav_fourcc('f', 'm', 't', ' '))
-			format = (const void *) &chunk[1];
-		else if (chunk_id == wav_fourcc('d', 'a', 't', 'a'))
-			break;
-
-		chunk = (const void *) ((const u8 *) &chunk[1] + chunk_size);
-	}
-
-	info->blocks = &chunk[1];
-	info->size = chunk_size;
-	info->sample_rate = ltoh32(format->sample_rate);
-	info->sample_format = ltoh16(format->format);
-	info->channel_count = ltoh16(format->channel_count);
-
-	info->frame_count = chunk_size / info->channel_count / ltoh16(format->bits_per_sample);
-
-	if (info->sample_format != WAV_FORMAT_INT16 &&
-			info->sample_format != WAV_FORMAT_FLOAT32)
-		return -3;
-
-	return 0;
-}
-
-static int wav_write(void *buffer, const struct wav_info *info) {
-	struct wav_chunk *chunk;
-	struct wav_riff *riff;
-	struct wav_format *format;
-	unsigned nbytes;
-
-	if (info->sample_format == WAV_FORMAT_INT16)
-		nbytes = 2;
-	else if (info->sample_format == WAV_FORMAT_FLOAT32)
-		nbytes = 4;
-	else
-		return -1;
-
-	chunk = buffer;
-	chunk->id = htob32(wav_fourcc('R', 'I', 'F', 'F'));
-	chunk->size = htol32(36 + info->size);
-
-	riff = (void *) &chunk[1];
-	riff->format = htob32(wav_fourcc('W', 'A', 'V', 'E'));
-
-	chunk = (void *) &riff[1];
-	chunk->id = htob32(wav_fourcc('f', 'm', 't', ' '));
-	chunk->size = 16;
-
-	format = (void *) &chunk[1];
-	format->format = htol16((u16) info->sample_format);
-	format->channel_count = htol16((u16) info->channel_count);
-	format->sample_rate = htol32((u32) info->sample_rate);
-	format->byte_rate = htol32((u32) (info->sample_rate * nbytes * info->channel_count));
-	format->block_align = htol16((u16) (nbytes * info->channel_count));
-	format->bits_per_sample = htol16(nbytes * 8);
-
-	chunk = (void *) &format[1];
-	chunk->id = htob32(wav_fourcc('d', 'a', 't', 'a'));
-	chunk->size = htol32((u32)  info->size);
-
-	return 0;
-}
+int wav_parse(struct wav_info *info, const void *data);
+int wav_write(void *buffer, const struct wav_info *info);
 
 #ifdef __cplusplus
 } /* extern "C" */
